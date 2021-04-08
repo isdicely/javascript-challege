615b3d035515582b4d48655e01039b1c70c6b123
isdicely <75281465+isdicely@users.noreply.github.com>
2021-04-07 23:34:54 -0700

    Added comments to code

--- UFO-level-1/static/index.js
+++ UFO-level-1/static/index.js
@@ -8,8 +8,9 @@ function make_date(str){
 };
 
 // Replace datetime string with a date
-// const defines a variable that can not be reassgined. Remains constant
+    // const defines a variable that can not be reassgined. Remains constant
 const ufo_data= data.map(entry=>({...entry,datetime:make_date(entry.datetime)}));
+// Make a display format of the data where the datetime displays better for the html site
 const display_data= ufo_data.map(entry=>({...entry,datetime:entry.datetime.toLocaleDateString()}));
 // Find earliest date and latest date in the data
 var earliest_sighting= new Date(Math.min(...ufo_data.map(sighting=>sighting.datetime)));
@@ -20,8 +21,10 @@ var instructions= d3.select(".instructions");
 instructions.text(`Please select a date (MM/DD/YYYY) between
  ${earliest_sighting.toLocaleDateString()} and ${latest_sighting.toLocaleDateString()}.`)
 
-// Select area in html where table will be built
+// Insert table into html
+    // Select area in html where table will be built
 var tbody= d3.select("tbody");
+    // Function to make table
 function make_table(data){
     data.forEach(function(sighting) {
         var row = tbody.append("tr");
@@ -31,6 +34,6 @@ function make_table(data){
         })
     })
 };
-
+// Display table
 make_table(display_data);
 
